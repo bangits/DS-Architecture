cd620ffcb5de6033e6e31025c0bd76dfb5994705
Armen Mesropyan <amesropyan@bangits.com>
2023-08-23 17:45:37 +0400

    fix: update ci/cd

--- packages/react/release.config.js
+++ packages/react/release.config.js
@@ -6,18 +6,17 @@ const config = {
     [
       "@semantic-release/npm",
       {
-        pkgRoot: "dist/react/src",
+        npmPublish: false,
       },
     ],
-    // [
-    //   "@semantic-release/git",
-    //   {
-    //     assets: ["dist/react/src/*.ts", "dist/react/src/*.tsx"],
-    //     message:
-    //       "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-    //   },
-    // ],
-    // "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
   ],
 };
 
